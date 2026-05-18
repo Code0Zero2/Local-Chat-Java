becf4324ff6d5e28dabadd1545859d3f1486ff3e
Morxidia <morxidia@gmail.com>
2026-05-18 17:56:48 +0700

    ref: refactor the code base to more readable code

---
+++ src/Local_chatapp/MultiClient.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Local_chatapp;
+
+import static Local_chatapp.MulticastClient.name;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Pinder
+ */
+public class MultiClient {
+    public MultiClient() { 
+        name= JOptionPane.showInputDialog("Please enter your name");
+        int count=0;
+        while(name==null || name.equals("") ){
+            if(name==null){   
+                new ChatApp().setVisible(true);
+                count++;
+                break;
+            }
+            else if(name.equals("")){   
+                JOptionPane.showMessageDialog(new ChatApp(), "Please enter a proper name");
+                name=JOptionPane.showInputDialog("Please enter your name");
+            }
+        }
+        if(count==0){ 
+            new MulticastClient().setVisible(true);
+            Thread t1=new Thread(new Client());
+            t1.start();
+       }
+    }
+}
